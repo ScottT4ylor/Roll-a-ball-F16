1f5fe3253c1e813b235a68e7d744ac88857c5622
ScottT4ylor <G00236985@smail.raritanval.edu>
2016-09-02 16:52:21 -0400

    Commit #3 - Added Player Movement
    Added script to control player movement.

---
+++ Assets/Scipts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	public float thrust;
+	public Rigidbody rb;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal * 3f, 0.0f, moveVertical * 3f);
+		rb.AddForce (movement);
+	}
+
+}
