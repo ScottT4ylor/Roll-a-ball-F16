7b6481716ddeb787ccf96c1f6a722672c3a64141
ScottT4ylor <G00236985@smail.raritanval.edu>
2016-09-08 22:14:18 -0400

    Commit #8 - Made score UI
    Score counts how many pickups the player gets, and displays winning
    message when they collect all of them.

--- Assets/Scipts/PlayerController.cs
+++ Assets/Scipts/PlayerController.cs
@@ -1,15 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
 	public float thrust;
 	public Rigidbody rb;
+    private int count;
+    public Text countText;
+    public Text winText;
 
 	void Start()
 	{
-		rb = GetComponent<Rigidbody> ();
+        count = 0;
+        setCountText();
+        winText.text = "";
+        rb = GetComponent<Rigidbody> ();
 	}
+
 	void FixedUpdate()
 	{
 		float moveHorizontal = Input.GetAxis ("Horizontal");
@@ -23,6 +31,16 @@ public class PlayerController : MonoBehaviour
         if(other.gameObject.tag == "Pickups")
         {
             other.gameObject.SetActive(false);
+            count++;
+            setCountText();
+        }
+    }
+    void setCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count>=9)
+        {
+            winText.text = "You win!";
         }
     }
 }
